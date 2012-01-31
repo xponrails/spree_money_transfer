8c969ee02a95a2b6d2db408038a040e8c62f8e9d
Stefano Mancini <stefano.mancini@devinterface.com>
2012-01-31 10:43:42 +0100

    some changes on payment

---
+++ app/models/order_decorator.rb
@@ -0,0 +1,62 @@
+Order.class_eval do
+
+  # order state machine (see http://github.com/pluginaweek/state_machine/tree/master for details)
+  state_machine :initial => 'cart', :use_transactions => false do
+
+    event :next do
+      transition :from => 'cart',     :to => 'address'
+      transition :from => 'address',  :to => 'delivery'
+      transition :from => 'delivery', :to => 'payment'
+      transition :from => 'confirm',  :to => 'complete'
+
+      # note: some payment methods will not support a confirm step
+      transition :from => 'payment',  :to => 'confirm',
+                                      :if => Proc.new { Gateway.current && Gateway.current.payment_profiles_supported? }
+
+      transition :from => 'payment', :to => 'complete'
+    end
+
+    event :cancel do
+      transition :to => 'canceled', :if => :allow_cancel?
+    end
+    event :return do
+      transition :to => 'returned', :from => 'awaiting_return'
+    end
+    event :resume do
+      transition :to => 'resumed', :from => 'canceled', :if => :allow_resume?
+    end
+    event :authorize_return do
+      transition :to => 'awaiting_return'
+    end
+
+    before_transition :to => 'complete' do |order|
+      begin
+        order.process_payments!
+      rescue Spree::GatewayError
+        if Spree::Config[:allow_checkout_on_gateway_error]
+          true
+        else
+          false
+        end
+      end
+      order.create_money_transfer!
+    end
+
+    after_transition :to => 'complete', :do => :finalize!
+    after_transition :to => 'delivery', :do => :create_tax_charge!
+    after_transition :to => 'payment', :do => :create_shipment!
+    after_transition :to => 'canceled', :do => :after_cancel
+
+  end
+
+  # Creates a money transfer adjustment
+  def create_money_transfer!
+    if self.payment_method.type == "PaymentMethod::MoneyTransfer"
+      # with money transfer shipment borns ready
+      self.shipment.ready
+      update_shipment_state
+      update_totals
+    end
+  end
+
+end
--- app/models/payment_method/money_transfer.rb
+++ app/models/payment_method/money_transfer.rb
@@ -11,4 +11,30 @@ class PaymentMethod::MoneyTransfer< PaymentMethod
   preference :send_email, :boolean
   preference :from_email, :string
   # To change this template use File | Settings | File Templates.
+
+  def actions
+    %w{capture void}
+  end
+
+  # Indicates whether its possible to capture the payment
+  def can_capture?(payment)
+    ['checkout', 'pending'].include?(payment.state)
+  end
+
+  # Indicates whether its possible to void the payment.
+  def can_void?(payment)
+    payment.state != 'void'
+  end
+
+  def capture(payment)
+    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
+    payment.complete
+    true
+  end
+
+  def void(payment)
+    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
+    payment.void
+    true
+  end
 end
